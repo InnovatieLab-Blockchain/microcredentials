a25af3afe7ddaa751f27f4e6af9ba71215748812
keeninsight <j.el.bazi@gmail.com>
2018-03-15 15:50:16 +0100

    Commit met(denk aan RUG + RUG)

--- src/js/uport.js
+++ src/js/uport.js
@@ -2,30 +2,23 @@ const Credentials = uportconnect.Credentials;
 const Connect = uportconnect.Connect;
 const SimpleSigner = uportconnect.SimpleSigner;
 
-// import Connect from 'uport-connect'
-// import SimpleSigner from 'uport-connect'
-// import Credentials from 'uport-connect'
+// import Connect from 'uport-duo_connector'
+// import SimpleSigner from 'uport-duo_connector'
+// import Credentials from 'uport-duo_connector'
 
-const connect = new Connect('DUO', {
+const duo_connector = new Connect('DUO', {
     clientId: '2oynp4geSgBwqkQebaYtexB32rCNbPmLu5K',
     network: 'rinkeby',
     signer: SimpleSigner('69c9446852693c00bd0a8825fad8297e4f9db34c9562a660585a0e767f993bd7')
 });
 
-const credentialsMC = new Credentials({
-    appName: 'Microcredentials-test',
-    network: 'rinkeby',
-    address: '2ovZg93ga1UZnfrzq1PyXCfmYbdmYjHryi6',
-    signer: SimpleSigner('03e880ceab681858cc09b1016fbb06d652a242d084d90f82807276e378be64d8')
-});
-
-const connectRug = new Connect('RUG', {
+const issuer_RUG = new Connect('RUG', {
     clientId: '2ojKNym3x16kUNQPq32CNcRpuo8MDH5w5vQ',
     network: 'rinkeby',
     signer: SimpleSigner('11cba7f19bce365f70b85e029f048f5c4cc73ad3232db828af5107dbb865e2b6')
 });
 
-const connectRabo = new Connect('Rabobank',{
+const verifier_RABO = new Connect('Rabobank',{
     clientId: '2otfT9XykJx5HJEquMhg4WTeLNKkx8ZkjBE',
     network: 'rinkeby',
     signer: SimpleSigner('13b1ed7eb7d0af503dd5f9e292356d58ece0e50221c3ba65ec8ce4c5a3b99c51')
@@ -39,17 +32,17 @@ const credentialsRabo = new Credentials({
 });
 
 // global variables to reuse
-let context = {};
+let user_data = {};
 
 // print credentials in console to test
 function show_credentials() {
-    console.log("Country = " + context.uportCountry);
-    console.log("Name = " + context.uportName)
+    console.log("Country = " + user_data.uportCountry);
+    console.log("Name = " + user_data.uportName)
 }
 
-// uPort connect
+// uPort request credentials
 const uportConnect = function () {
-    connect.requestCredentials({
+    duo_connector.requestCredentials({
         requested: ['name', 'phone', 'country', 'avatar'],
         // callbackUrl:'student.html',
         notifications: true // We want this if we want to recieve credentials
@@ -58,18 +51,19 @@ const uportConnect = function () {
             // Do something (in this case print all credentials)
             console.log("Credentials:", credentials);
 
-            context.uportId = credentials.address;
-            context.uportName = credentials.name;
-            context.uportCountry = credentials.country;
-            context.uportPhone = credentials.phone;
+            user_data.uportId = credentials.address;
+            user_data.uportName = credentials.name;
+            user_data.uportCountry = credentials.country;
+            user_data.uportPhone = credentials.phone;
             // window.location.href = "student.html";
         })
 };
 
+// attest/issue badge
 const uportAttest = function () {
-    connectRug.attestCredentials({
-        sub: context.uportId,
-        claim: {OpenBadge1: {naam: 'OpenBadge1', description: 'Atheneum Diploma 2018' }},
+    issuer_RUG.attestCredentials({
+        sub: user_data.uportId,
+        claim: {Bachelor_Of_Education: {naam: 'Bachelor_Of_Education', description: 'Bachelor_Of_Education 2018' }},
         // callbackUrl: 'student2.html',
         exp: new Date().getTime() + 30 * 24 * 60 * 60 * 1000, // 30 days from now
         // uriHandler: (log) => { console.log(log)}
@@ -81,8 +75,8 @@ const uportAttest = function () {
 };
 
 const uportVerify = function () {
-    connectRabo.requestCredentials({
-        verified: ['OpenBadge1'],
+    verifier_RABO.requestCredentials({
+        verified: ['Bachelor_Of_Education'],
         notifications: true,
         exp: new Date().getTime() + 60000
     }).then((profile) => {
